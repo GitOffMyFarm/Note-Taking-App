1e920e6c5e690e5fbd72c32d5bb610a31fabf1ae
Joseph Freeman <freemanjj1992@gmail.com>
2021-05-05 16:40:57 -0400

    added start script

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
+//const db = require('./db/db.json')
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+//app.get('/api/notes', (req, res) => {
+    //res.json(path.join(db))});
 app.listen(PORT, () => console.log(`You are now live at Port:${PORT}`));
