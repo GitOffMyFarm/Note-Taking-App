4ddea6c9c194ba96da8ebe3d92d9c4332f87e07f
Joseph Freeman <freemanjj1992@gmail.com>
2021-05-05 16:43:31 -0400

    added dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 //const db = require('./db/db.json')
 
 app.use(express.static(__dirname + '/public'));
